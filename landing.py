bd4819b99b13fff28410f51eb52abb02f0e261cf
Fernando Sanchez-Monge Perez <fernandosanchezmongeperez@MacBook-Air-de-Fernando.local>
2024-12-22 21:44:39 +0100

    image background

--- landing.py
+++ landing.py
@@ -1,28 +1,27 @@
 import streamlit as st
 
-def main():
-    page_bg_img = """
-    <style>
-    /* Override the default body background */
-    body {
-        background-image: url("https://images.unsplash.com/photo-1612831455549-43e4f05e4f9a");
-        background-size: cover;
-        background-repeat: no-repeat;
-        background-attachment: fixed;
-    }
-    </style>
-    """
-    # Create a centered container for the sign-up form
-    with st.container():
-        # Create the signup form using Streamlit's form
-        with st.form("signup_form"):
-            name = st.text_input("Username")
-            email = st.text_input("Email")
-            password = st.text_input("Password", type="password")
+raw_url = "https://github.com/fsanchezmonge/training-kpis/blob/oauth-flow/background_img.png"
+page_bg_img = """
+<style>
+[data-testid="stAppViewContainer"] {
+background-image: url("https://github.com/fsanchezmonge/training-kpis/blob/oauth-flow/background_img.png?raw=true");
+background-size: cover;
+;
+}
+[data-testid="stHeader"]{
+background-color: rgba(0,0,0,0);
+}
+</style>
+"""
+st.markdown(page_bg_img, unsafe_allow_html=True)
 
-            submitted = st.form_submit_button()
-            if submitted:
-                st.success(f"Thank you for signing up, {name}!")
+with st.container():
+    # Create the signup form using Streamlit's form
+    with st.form("signup_form"):
+        name = st.text_input("Username")
+        email = st.text_input("Email")
+        password = st.text_input("Password", type="password")
 
-if __name__ == "__main__":
-    main()
+        submitted = st.form_submit_button()
+        if submitted:
+            st.success(f"Thank you for signing up, {name}!")
